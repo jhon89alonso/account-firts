57ba8a0ec9d001a4e6c098f968bc73b1d76dff88
Jhon Alonso <jhon2alonso@gmail.com>
2023-10-11 18:29:12 -0500

    start project laravel, vue, vite and plantilla vue-notus

---
+++ resources/js/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="app">
+        <router-view />
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum,
+            laboriosam rerum eius aliquam voluptas corporis expedita quibusdam
+            blanditiis, eaque et tempore, quisquam inventore consectetur? Alias
+            minus quisquam voluptatibus laboriosam tempora.
+        </p>
+    </div>
+</template>
+<!-- <script>
+export default {
+    name: "Test",
+    data() {
+        return {
+            count: 0,
+        };
+    },
+};
+</script> -->
